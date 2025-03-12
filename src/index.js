1c79e38ac1bcda6c79f969649af2aa91827e358e
wdm-coding <wdmcoding0620@163.com>
2025-03-12 17:46:59 +0800

    微调

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AdminApp from '@/admin-page/AdminApp.js';
 import '@ant-design/v5-patch-for-react-19';
 import store from './store';
 import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider  store={store} >
-    <App></App>
+    <AdminApp></AdminApp>
   </Provider>
 );
